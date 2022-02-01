9e48f56d2f842b7f841a69eba2e0cc2a3ce7142f
Majid <Majiadted@gmail.com>
2022-02-01 19:27:15 +0330

    refactor: `store` module use new `store` model

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Store, StoreSchema } from './schemas/store.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Store.name, schema: StoreSchema, collection: 'stores' },
+    ]),
+  ],
+})
 export class StoreModule {}
